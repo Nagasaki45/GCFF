f7163031c2e66dbef7ae4509de5875f394f2bbcc
Tom Gurion <nagasaki45@gmail.com>
2017-06-10 20:37:15 +0100

    Use oct2py to call the algorithm from python

---
+++ gcff.py
@@ -0,0 +1,33 @@
+"""
+A python wrapper around the GCFF algorithm (originally in Matlab, see gc.m).
+
+For more information refer to the paper
+http://vips.sci.univr.it/research/fformation/.
+"""
+
+from oct2py import octave
+
+def gc(features , stride, mdl):
+    """
+    F-Formation analysis using the GCFF algorithm.
+
+    features: a N*4 numpy.ndarray with rows representing
+              [id, x_position, y_position, orientation].
+              orientation is given in radians.
+    stride: the distance between the individual i and the centre
+            of its transactional segment.
+    mdl: a minimum description length prior, linearly penalising the
+         log-likelihood for the number of models used.
+
+    Returns a vector of length N with the o-space assignment of each
+    participant.
+    """
+
+    stride = float(stride)
+    mdl = float(mdl)
+    result = octave.gc(features, stride, mdl)
+    # Octave returns a n*1 column (2 dimensional in numpy terms)
+    result = result.T[0]
+    # It came back as np.float64, should be ints
+    result = result.astype(int)
+    return result
--- vis_group.py
+++ vis_group.py
@@ -6,6 +6,8 @@ Created on Mon Jul  1 17:16:31 2013
 """
 import numpy as np
 from scipy import io
+import gcff
+
 path = 'data/Synth/'
 a=io.loadmat(path+'features.mat')
 f=a['features'][0]
@@ -131,79 +133,14 @@ def find_locs(f,stride=35):
     locs[:,0]=f[:,1]+np.cos(f[:,3])*stride
     locs[:,1]=f[:,2]+np.sin(f[:,3])*stride
     return locs
-    
-def calc_distance_old(loc,labels,mdl):
-    u=np.unique(labels)
-    dist=np.empty((loc.shape[0],u.shape[0]))
-    dist2=np.zeros_like(dist)
-    for i in range(u.shape[0]):
-        means=loc[labels==i,:].mean(0)
-        disp=loc-means
-        dist[:,i]=(disp**2).sum(1)
-        mask=np.arange(loc.shape[0])[dist[:,i]<mdl]
-        for j in mask:
-            for k in mask:
-                if dist[k,i]>dist[j,i]:
-                    inner=disp[k].dot(disp[j])
-                    norm=np.sqrt(dist[k,i]*dist[j,i])
-                    if inner/norm>.9:
-                        dist2[k,i]+=100**(dist[k,i]/dist[j,i])
-        dist+=dist2
-    return dist
 
-
-def calc_distance(loc,f,labels,mdl):
-    """Given focal localtions, raw locations(f) and initial labelling l find
-    cost of assigning  people to new locations given by the mean of their 
-    labelling"""           
-    u=np.unique(labels)
-    dist=np.empty((loc.shape[0],u.shape[0]))
-    for i in range(u.shape[0]):
-        means=loc[labels==i,:].mean(0)      
-        dist[:,i]=((loc-means)**2).sum(1)
-        #computed sum-squares distance, now
-        mask=np.arange(loc.shape[0])[dist[:,i]<mdl]
-        disp=f[:,1:3].copy()
-        disp-=means
-        for j in mask:
-            for k in mask:
-                distk=np.linalg.norm(disp[k])
-                distj=np.linalg.norm(disp[j]) 
-                if distk>distj:
-                    inner=disp[k].dot(disp[j])
-                    norm=distk*distj
-                    if inner/norm>.75:
-                        dist[k,i]+=100**(inner/norm*distk/distj)
-    return dist
-def init(locs,f,mdl):
-    return calc_distance(locs,f,np.arange(locs.shape[0]),mdl)
-
-def gc(f,stride=35,MDL=3500):
-    """Runs graphcuts"""
-    locs=find_locs(f,stride)
-    unary=init(locs,f,MDL)
-    blank=np.zeros((f.shape[0],0),dtype=np.double)
-    neigh=blank
-    weight=blank
-    seg=np.arange(f.shape[0],dtype=np.double)
-    for i in range(5):
-        mdl=np.ones(unary.shape[1])*MDL
-        #Run Graph-cuts
-        #_,seg=segmentation.expand(unary,neigh,weight,mdl,seg.astype(np.double))
-        #discard unused labells
-        seg=seg.astype(np.int)
-        _,seg=np.unique(seg,return_inverse=True)
-        #refit distances
-        unary=calc_distance(locs,f,seg,MDL)
-    return seg
 
 def make_est(f,stride=35,mdl=3500):
     """Solve entire sequence"""
     est=np.empty(f.shape[0],dtype=object)
     for i in range(f.shape[0]):
-        est[i]=gc(f[i],stride,mdl)
+        est[i]=gcff.gc(f[i],stride,mdl)
     return est
-    
 
 est=make_est(f,stride=25,mdl=3000)
 
